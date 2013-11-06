f5814f774983eaf9efb7e22460e609f438cc2dab
Ivan Baguio <baguio.ivan@yahoo.com>
2013-11-07 04:12:57 +0800

    updates

--- others/fresh-install.sh
+++ others/fresh-install.sh
@@ -2,7 +2,7 @@
 
 echo "Starting installation of complete wifination backend server"
 apt-get update
-apt-get install -y freeradius freeradius-mysql mysql-server python-pip apache2 nmap git
+apt-get install -y freeradius freeradius-mysql mysql-server python-pip apache2 nmap git libapache2-mod-wsgi
 pip install flask
 
 clear
@@ -11,12 +11,18 @@ echo "Downloading frontend and configuring apache"
 #install apache2 and webserver stuff
 cd ~/
 git clone https://github.com/ibaguio/wifination-server
+mkdir ~/wifination-server/http
+mkdir ~/wifination-server/logs
+
 cp ~/wifination-server/others/sites-available-wifination /etc/apache2/sites-available/wifination
-ln -l /etc/apache2/sites-available/wifination-server /etc/apache2/sites-enabled/wifination-server
+ln -s /etc/apache2/sites-available/wifination /etc/apache2/sites-enabled/wifination
 rm /etc/apache2/sites-available/default*
 
 a2dissite default
 a2ensite wifination
+a2enmod wsgi
+adduser wifination
+passwd -d wifination
 echo "Restarting apache"
 service apache2 restart
 

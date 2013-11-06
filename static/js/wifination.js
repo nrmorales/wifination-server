e7474ab9428df61ef90941fbabc349d6e35fa70e
Ivan Baguio <baguio.ivan@yahoo.com>
2013-11-07 02:51:53 +0800

    reverted to userpass

--- static/js/wifination.js
+++ static/js/wifination.js
@@ -14,3 +14,8 @@ function submitForm(){
    }
    if (ok){$("form#login_form").submit()}
 }
+$("button#btn-login").click(function(){
+   $('div#input_.hide').fadeIn("slow").removeClass('hide');
+   $("button#btn-login").text("Login Now!");
+   $("button#btn-login").attr('onclick','').click(submitForm);
+});
